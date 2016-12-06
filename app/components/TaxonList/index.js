9c76cb76d6d042b2a3b92bd40ca07a59d80490dc
Maksym Fedenko <maksym.fedenko@gmail.com>
2016-12-06 18:31:38 +0200

    start refactor taxonList

--- app/components/TaxonList/index.js
+++ app/components/TaxonList/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './styles.scss';
 
 const getTaxonomyTemplate = (taxonomy) => {
@@ -9,24 +9,51 @@ const getTaxonomyTemplate = (taxonomy) => {
   ];
 };
 
-export const TaxonList = ({ taxons } /* , { location } */) => (
-  <ul className={styles.taxonomyList}>
-    {taxons && taxons.length > 0 ?
-      taxons.map((taxonomy) => getTaxonomyTemplate(taxonomy))
-      :
-        <li className={styles.taxon}>No filter existed</li>
+
+export default class TaxonList extends Component {
+  static propTypes = {
+    taxons: PropTypes.array,
+  };
+
+  /*
+  static contextTypes = {
+   location: PropTypes.object,
+  }
+  */
+
+  state = {
+    selectStack: [],
+  };
+
+  renderTaxonList = () => this.selectStack.length > 0 ? this.renderSelectedTaxonList() : this.renderInitialTaxonList();
+
+  renderInitialTaxonList = () => this.props.taxons.filter(taxon => !taxon.parent_id).map(taxon => <li key={taxon.id} className={styles.taxon}>{taxon.name}</li>);
+
+  renderSelectedTaxonList = () => {
+    const {taxons} = this.props;
+    const {selectedStack} = this.state;
+    const result = [];
+
+    if (selectedStack.length > 1) {
+      result.push(<li key='reset' className={styles.taxon} onClick={() => this.setState({selectedStack: []})}>Reset</li>)
     }
-  </ul>
-);
 
-/*
-TaxonList.contextTypes = {
-  location: PropTypes.object,
+    return result;
+  };
+
+  render () {
+    const { taxons } = this.props;
+
+    return (
+      <ul className={styles.taxonomyList}>
+        {taxons && taxons.length > 0 ?
+          this.renderTaxonList()
+          :
+          <li className={styles.taxon}>No filter existed</li>
+        }
+      </ul>
+    )
+  }
 }
-*/
 
-TaxonList.propTypes = {
-  taxons: PropTypes.array,
-};
 
-export default TaxonList;
